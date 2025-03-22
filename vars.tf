1c47e3bace9875158bb3d4cbee05d2d59525f851
nathaliaifurita <nathaliaofreire@gmail.com>
2025-03-22 12:06:50 -0300

    Alterando ARN 2

--- vars.tf
+++ vars.tf
@@ -23,7 +23,7 @@ variable "instanceType" {
 }
 
 variable "principalArn" {
-  default = "arn:aws:iam::340103052246a:role/voclabs"
+  default = "arn:aws:iam::340103052246:role/voclabs"
 }
 
 variable "policyArn" {
