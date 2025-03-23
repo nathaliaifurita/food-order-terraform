e6641a9da35d80807311c3fedc5452cad7de1573
nathaliaifurita <nathaliaofreire@gmail.com>
2025-03-23 18:30:24 -0300

    WIP

--- provider.tf
+++ provider.tf
@@ -6,6 +6,10 @@ data "aws_eks_cluster" "eks_cluster" {
   name = var.projectName
 }
 
+data "aws_eks_cluster_auth" "eks_auth" {
+  name = var.projectName
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
