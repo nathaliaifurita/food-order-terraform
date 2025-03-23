779ead9c2eea62a77d90cbcc9d71d6d51b22066b
nathaliaifurita <nathaliaofreire@gmail.com>
2025-03-23 18:18:58 -0300

    Garantindo que ta apontando pro eks

--- provider.tf
+++ provider.tf
@@ -2,15 +2,21 @@ provider "aws" {
   region = var.regionDefault
 }
 
+data "aws_eks_cluster" "eks_cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "eks_auth" {
+  name = var.cluster_name
+}
+
 provider "kubernetes" {
-  host                   = aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.name]
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks_cluster.name]
   }
-
-  depends_on    = [aws_eks_cluster.eks_cluster]
 }
