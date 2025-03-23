520b3ae15cd7b7906e4c3313d3accd40dd8c8c65
Diego G. lima <diego.lima@alice.com>
2025-03-23 15:11:39 -0300

    turn apigateway public

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,17 +1,10 @@
 resource "aws_api_gateway_rest_api" "food_order_api" {
   name        = "food-order-vpc-link"
   description = "API Gateway para o Food Order API"
-}
 
-resource "aws_api_gateway_vpc_link" "food_order" {
-  name        = "food-order-vpc-link"
-  target_arns = [aws_lb.food_order_lb.arn]
-  description = "VPC Link para o Food Order API"
-
-  depends_on = [
-    aws_lb.food_order_lb,
-    aws_lb_listener.http
-  ]
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
 resource "aws_api_gateway_resource" "proxy" {
@@ -20,11 +13,21 @@ resource "aws_api_gateway_resource" "proxy" {
   path_part   = "{proxy+}"
 }
 
+resource "aws_api_gateway_vpc_link" "food_order" {
+  name        = "food-order-vpc-link"
+  description = "VPC Link para o Food Order API"
+  target_arns = [aws_lb.food_order_lb.arn]
+}
+
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.food_order_api.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
-  authorization = "NONE"
+  authorization = "NONE"  # Confirmar que está NONE
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
 }
 
 resource "aws_api_gateway_integration" "load_balancer" {
@@ -36,10 +39,78 @@ resource "aws_api_gateway_integration" "load_balancer" {
   connection_type         = "VPC_LINK"
   connection_id           = aws_api_gateway_vpc_link.food_order.id
   uri                     = "http://${aws_lb.food_order_lb.dns_name}/{proxy}"
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+}
+
+resource "aws_api_gateway_method" "options" {
+  rest_api_id   = aws_api_gateway_rest_api.food_order_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.food_order_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration" "options" {
+  rest_api_id = aws_api_gateway_rest_api.food_order_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.options.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.food_order_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = aws_api_gateway_method_response.options.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT,DELETE'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
 }
 
 resource "aws_api_gateway_deployment" "food_order_api" {
-  depends_on = [aws_api_gateway_integration.load_balancer]
+  depends_on = [
+    aws_api_gateway_integration.load_balancer,
+    aws_api_gateway_integration.options
+  ]
+  
   rest_api_id = aws_api_gateway_rest_api.food_order_api.id
   stage_name  = "prod"
+}
+
+resource "aws_api_gateway_rest_api_policy" "api_policy" {
+  rest_api_id = aws_api_gateway_rest_api.food_order_api.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "execute-api:Invoke"
+        Resource = "${aws_api_gateway_rest_api.food_order_api.execution_arn}/*"
+      }
+    ]
+  })
 }
