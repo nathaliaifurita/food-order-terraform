a82f3c2c028b37ceb8ea196dbb98e1519d5a3806
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-01 23:57:39 -0300

    Atualizando projectNames

--- eks-access-entry.tf
+++ eks-access-entry.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_access_entry" "eks_access_entry" {
-  for_each = toset(var.project_names)
+  for_each = toset(var.projectNames)
 
   cluster_name  = aws_eks_cluster.eks_cluster[each.key].name
   principal_arn = var.principalArn
