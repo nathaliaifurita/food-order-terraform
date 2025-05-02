84df25c790c0ea557769785edd1189e304c3091a
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-01 23:38:50 -0300

    Atualizando eks-cluster

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,7 +1,9 @@
-resource "aws_eks_cluster" "eks-cluster" {
-  name     = var.projectNames
+resource "aws_eks_cluster" "eks_cluster" {
+  for_each = toset(var.project_names)
+
+  name     = "eks-${each.key}"  # Aqui sim é uma string
   role_arn = var.labRole
-  
+
   vpc_config {
     subnet_ids         = aws_subnet.private_subnets[*].id
     security_group_ids = [aws_security_group.sg.id]
@@ -9,5 +11,9 @@ resource "aws_eks_cluster" "eks-cluster" {
 
   access_config {
     authentication_mode = var.accessConfig
+  }
+
+  tags = {
+    Project = each.key
   }
 }
