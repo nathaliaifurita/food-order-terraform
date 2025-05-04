11550442ed02adc52498288720e6478fc9718a52
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-04 11:05:42 -0300

    Adicionando for_each no subnets

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,9 @@
 output "private_subnet_ids" {
+  for_each = local.indexed_projects
   value = aws_subnet.private_subnets[each.key].id
 }
 
 output "public_subnet_ids" {
+  for_each = local.indexed_projects
   value = aws_subnet.public_subnets[each.key].id
 }
