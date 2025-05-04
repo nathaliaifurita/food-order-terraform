ba45aec3e2db0de7a5a990c391b6bcb51f9d90d8
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-04 11:47:13 -0300

    Alterando locals

--- locals.tf
+++ locals.tf
@@ -30,11 +30,7 @@ locals {
 ###############################
 
 data "aws_availability_zones" "available" {
-  "us-east-1a",
-  "us-east-1b",
-  "us-east-1c",
-  "us-east-1d",
-  "us-east-1f"
+  state = "available"
 }
 
 data "aws_vpc" "main" {
