325964ab4e880e3ed58a3ac71b0cfb0d21d273a5
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-05 13:51:05 -0300

    Adicionando permissao pra role no fargate

--- fargate.tf
+++ fargate.tf
@@ -2,14 +2,37 @@ resource "aws_ecs_cluster" "auth_cluster" {
   name = "auth-cluster"
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "auth_task" {
   family                   = "auth-task"
   requires_compatibilities = ["FARGATE"]
   network_mode            = "awsvpc"
   cpu                     = 256
   memory                  = 512
-  execution_role_arn      = var.labRole
-  task_role_arn           = var.labRole
+  #execution_role_arn      = var.labRole
+  #task_role_arn           = var.labRole
+  execution_role_arn      = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn           = aws_iam_role.ecs_task_execution_role.arn
+
   runtime_platform {
     operating_system_family = "LINUX"
     cpu_architecture       = "ARM64"
