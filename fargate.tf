f42802ccdd9b130cc223a246fc15c6e1bd60ca06
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-05 16:37:43 -0300

    Atualizando fargate e locals

--- fargate.tf
+++ fargate.tf
@@ -1,3 +1,66 @@
+resource "aws_ecs_cluster" "auth_cluster" {
+  name = "auth-cluster"
+}
+
+resource "aws_ecs_task_definition" "auth_task" {
+  family                   = "auth-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode            = "awsvpc"
+  cpu                     = 256
+  memory                  = 512
+  execution_role_arn      = "arn:aws:iam::198212171636:role/ecsTaskExecutionRole"
+  task_role_arn           = "arn:aws:iam::198212171636:role/ecsTaskExecutionRole"
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture       = "ARM64"
+  }
+
+  container_definitions = jsonencode([
+    {
+      name  = "auth-container"
+      image = "diegogl12/auth_cpf:latest"
+      portMappings = [
+        {
+          containerPort = 4000
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name  = "PORT"
+          value = "4000"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/auth-service"
+          "awslogs-region"        = var.regionDefault
+          "awslogs-stream-prefix" = "auth"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_cloudwatch_log_group" "auth_service" {
+  name              = "/ecs/auth-service"
+  retention_in_days = 14
+}
+
+# Esta parte de Load Balancer e Listener deve ser removida do local.tf para evitar duplicação
+resource "aws_lb" "auth" {
+  name               = "auth-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [for s in aws_subnet.public_subnets : s.id]
+
+  tags = {
+    Name = "auth-lb"
+  }
+}
+
 resource "aws_lb_target_group" "auth_tg" {
   name     = "auth-tg"
   port     = 80
@@ -20,7 +83,7 @@ resource "aws_lb_target_group" "auth_tg" {
 }
 
 resource "aws_lb_listener" "auth" {
-  load_balancer_arn = aws_lb.main.arn
+  load_balancer_arn = aws_lb.auth.arn
   port              = 80
   protocol          = "HTTP"
 
@@ -29,10 +92,31 @@ resource "aws_lb_listener" "auth" {
     target_group_arn = aws_lb_target_group.auth_tg.arn
   }
 
-  depends_on = [aws_lb.main]
+  depends_on = [aws_lb.auth]
 }
 
 resource "aws_ecs_service" "auth_service" {
   name            = "auth-service"
   cluster         = aws_ecs_cluster.auth_cluster.id
-  task_definition_
+  task_definition = aws_ecs_task_definition.auth_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  depends_on = [aws_lb_listener.auth]
+
+  network_configuration {
+    subnets          = [for subnet in values(aws_subnet.private_subnets) : subnet.id]
+    security_groups  = [aws_security_group.fargate_sg.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.auth_tg.arn
+    container_name   = "auth-container"
+    container_port   = 4000
+  }
+
+  tags = {
+    Name = "auth-service"
+  }
+}
--- locals.tf
+++ locals.tf
@@ -148,36 +148,3 @@ resource "aws_lb" "auth" {
   }
 }
 
-resource "aws_lb_target_group" "auth_tg" {
-  name     = "auth-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main_vpc.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-
-  tags = {
-    Name = "auth-target-group"
-  }
-}
-
-resource "aws_lb_listener" "auth" {
-  load_balancer_arn = aws_lb.auth.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.auth_tg.arn
-  }
-
-  depends_on = [aws_lb.auth]
-}
