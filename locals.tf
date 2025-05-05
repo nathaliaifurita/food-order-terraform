c17dc780ea665afb557a31304110fe6a246288f6
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-05 17:25:22 -0300

    Atualiznado o ip da vpc

--- locals.tf
+++ locals.tf
@@ -60,7 +60,7 @@ locals {
   project_names_map    = zipmap(["auth", "pagamento", "pedido", "cardapio", "usuario"], ["auth", "pagamento", "pedido", "cardapio", "usuario"])
   indexed_projects      = zipmap(var.projectNames, range(length(var.projectNames)))
   availability_zones    = data.aws_availability_zones.available.names
-  vpc_cidr              = "10.0.0.0/16"
+  vpc_cidr              = "172.31.0.0/16"
   private_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnet_cidrs   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
