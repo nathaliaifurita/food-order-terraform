7016e19128ff564f11712c18d0bcce45cd471835
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-06 14:53:24 -0300

    Atualizando policy

--- apigw-policy.tf
+++ apigw-policy.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_role_policy_attachment" "attach_authorizer_policy" {
-  role          = var.labRole
+  role          = basename(var.labRole)
   policy_arn    = var.policyArnApiGatewayAuthorizer
 }
