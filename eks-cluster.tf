211a3eb7e016678231c9170aa8effe68fde89329
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-06 15:59:42 -0300

    Adicionando os addons

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -28,3 +28,33 @@ resource "aws_eks_cluster" "eks_cluster" {
     "authenticator"
   ]
 }
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  addon_name   = "kube-proxy"
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  addon_name   = "vpc-cni"
+}
+
+resource "aws_eks_addon" "eks-node-monitoring-agent" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  addon_name   = "eks-node-monitoring-agent"
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  addon_name   = "coredns"
+  addon_version = "v1.11.4-eksbuild.2"
+
+  depends_on = [
+    aws_eks_node_group.eks-node
+  ]
+}
+
+resource "aws_eks_addon" "eks-pod-identity-agent" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  addon_name   = "eks-pod-identity-agent"
+}
