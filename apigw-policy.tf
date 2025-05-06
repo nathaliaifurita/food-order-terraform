f9258ab2ed02e69b85eb99ceab9766a9cf64ad9c
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-06 15:02:21 -0300

    Atualizando policy

--- apigw-policy.tf
+++ apigw-policy.tf
@@ -1,4 +1,19 @@
-resource "aws_iam_role_policy_attachment" "attach_authorizer_policy" {
-  role          = basename(var.labRole)
-  policy_arn    = var.policyArnApiGatewayAuthorizer
+resource "aws_iam_role" "apigw_authorizer_role" {
+  name = "APIGatewayAuthorizerRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "apigateway.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.apigw_authorizer_role.name
+  policy_arn = var.policyArnApiGatewayAuthorizer
 }
