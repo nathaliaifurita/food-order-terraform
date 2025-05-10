242defcd7de3e2a3a9a5e7e3838b4214a21d6d61
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-10 13:34:59 -0300

    Alterando count

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "eks_cluster_name" {
+  count = length(var.projectNames)
   value = aws_eks_cluster.eks_cluster[count.index].name
 }
 
@@ -7,17 +8,21 @@ output "security_group_id" {
 }
 
 output "eks_cluster_endpoint" {
+  count = length(var.projectNames)
   value = aws_eks_cluster.eks_cluster[count.index].endpoint
 }
 
 output "eks_cluster_certificate_authority_0_data" {
+  count = length(var.projectNames)
   value = aws_eks_cluster.eks_cluster[count.index].certificate_authority[0].data
 }
 
 output "private_subnet_ids" {
+  count = length(var.projectNames)
   value = aws_subnet.private_subnets[count.index].id
 }
 
 output "public_subnet_ids" {
+  count = length(var.projectNames)
   value = aws_subnet.public_subnets[count.index].id
 }
