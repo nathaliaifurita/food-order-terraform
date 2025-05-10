e7e34da6da26ddd67237f656dfcfdc613184cccd
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-10 11:26:36 -0300

    Alterando output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,5 @@
 output "eks_cluster_name" {
-  for_each = toset(var.projectNames)
-  value = aws_eks_cluster.eks_cluster[each.key].name
+  value = aws_eks_cluster.eks_cluster[*].name
 }
 
 output "security_group_id" {
@@ -8,13 +7,11 @@ output "security_group_id" {
 }
 
 output "eks_cluster_endpoint" {
-  for_each = toset(var.projectNames)
-  value = aws_eks_cluster.eks_cluster[each.key].endpoint
+  value = aws_eks_cluster.eks_cluster[*].endpoint
 }
 
 output "eks_cluster_certificate_authority_0_data" {
-  for_each = toset(var.projectNames)
-  value = aws_eks_cluster.eks_cluster[each.key].certificate_authority[0].data
+  value = aws_eks_cluster.eks_cluster[*].certificate_authority[0].data
 }
 
 output "private_subnet_ids" {
