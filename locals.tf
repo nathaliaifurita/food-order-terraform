0bbbb5916078eb6e6b310bd4eb7959d7e83ecf57
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-10 11:17:35 -0300

    Adicionando o count e alterando a chamada de eks_cluster nos outputs

--- locals.tf
+++ locals.tf
@@ -68,6 +68,7 @@ resource "aws_subnet" "public_subnets" {
 }
 
 resource "aws_subnet" "private_subnets" {
+  count             = 2
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet("172.31.0.0/16", 4, count.index)
   availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
@@ -75,7 +76,7 @@ resource "aws_subnet" "private_subnets" {
   tags = {
     Name        = "Private Subnet ${count.index + 1}"
     Environment = "private"
-    "kubernetes.io/cluster/${var.projectName}" = "shared"
+    "kubernetes.io/cluster/${var.projectNames}" = "shared"
     "kubernetes.io/role/internal-elb"         = "1"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "eks_cluster_name" {
-  value = aws_eks_cluster.eks-cluster.name
+  value = aws_eks_cluster.eks_cluster.name
 }
 
 output "security_group_id" {
@@ -7,9 +7,9 @@ output "security_group_id" {
 }
 
 output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.eks-cluster.endpoint
+  value = aws_eks_cluster.eks_cluster.endpoint
 }
 
 output "eks_cluster_certificate_authority_0_data" {
-  value = aws_eks_cluster.eks-cluster.certificate_authority[0].data
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 }
