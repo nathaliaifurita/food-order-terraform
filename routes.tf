1f5906946f2ea2af1876a030441385ec4a810736
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-10 13:12:09 -0300

    Removendo todos os each

--- routes.tf
+++ routes.tf
@@ -29,7 +29,7 @@ resource "aws_route_table" "private" {
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
   }
 
   tags = {
