d72403eba2623f6b2ccc64f3d1858caa18adb3d0
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-11 13:10:25 -0300

    Atualizando allocation id

--- locals.tf
+++ locals.tf
@@ -57,7 +57,7 @@ resource "aws_subnet" "public_subnets" {
   count                   = 2
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet("10.0.1.0/24", 4, count.index)
-  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+  availability_zone       = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
   map_public_ip_on_launch = true
 
   tags = merge (
@@ -74,7 +74,7 @@ resource "aws_subnet" "private_subnets" {
   count             = 2
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet("10.0.2.0/24", 4, count.index + 2)
-  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
+  availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
 
   tags = merge(
     {
@@ -103,8 +103,7 @@ resource "aws_eip" "nat" {
 }
 
 resource "aws_nat_gateway" "nat" {
-  count         = 2
-  allocation_id = aws_eip.nat.id
+  allocation_id = "${aws_eip.nat.id}"
   subnet_id     = aws_subnet.public_subnets[count.index].id
 
   tags = {
