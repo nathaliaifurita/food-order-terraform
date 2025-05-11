14c67ae27d0c7d4b7ea21f60eaaa9ad7fd6c83c9
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-11 12:32:16 -0300

    Atualizando tabela de rotas

--- routes.tf
+++ routes.tf
@@ -20,15 +20,16 @@ resource "aws_route_table_association" "public" {
 # Private Route Table
 # -------------------
 resource "aws_route_table" "private" {
-  count  = 2
+  count  = length(var.private_subnet_cidrs)
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = nat_gateway_id = aws_nat_gateway.nat[count.index].id
   }
 
   tags = {
-    Name = "private-rt"
+    Name = "private-route-table-${count.index}"
   }
 }
 
