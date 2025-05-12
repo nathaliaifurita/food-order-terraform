3644851ed782b85c1b013a6a289ce9ffce955a34
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-11 21:57:48 -0300

    Alterando policy

--- eks-access-policy.tf
+++ eks-access-policy.tf
@@ -7,3 +7,24 @@ resource "aws_eks_access_policy_association" "eks-access-policy" {
     type = "cluster"
   }
 }
+
+resource "aws_iam_policy" "eks_passrole_labrole" {
+  name        = "AllowPassRoleLabRole"
+  description = "Allow passrole for EKS LabRole"
+  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "iam:PassRole",
+        Resource = "arn:aws:iam::198212171636:role/LabRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_passrole_to_voclabs" {
+  role       = "voclabs"
+  policy_arn = aws_iam_policy.eks_passrole_labrole.arn
+}
